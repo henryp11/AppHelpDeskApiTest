28b3dc5fb9b886a76bca55a328d9237a858d342a
henryR <henry11snake@hotmail.com>
2024-05-28 13:55:39 -0500

    Se quitás los comentarios para documentación ya que al parecer están causando conflicto al subir a railway

--- src/utils/auth/index.js
+++ src/utils/auth/index.js
@@ -2,10 +2,9 @@
 const passport = require('passport');
 
 //Aquí declaro todas las estrategias de auth que voy a utilizar
-/**Estrategia para validar usuario y contraseña en base de datos. {@link module:Passport_Local}*/
 const LocalStrategy = require('./strategies/localStrategy');
 
-/**Estrategia para validar token Jwt una vez logueado. {@link module:Passport_JWT}*/
+//Estrategia para validar token Jwt una vez logueado.
 const JwtStrategy = require('./strategies/jwtStrategy');
 
 passport.use(LocalStrategy);
