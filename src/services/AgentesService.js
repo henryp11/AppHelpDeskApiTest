a900ac6b73b73e7796b97ecb4259bbcd0926c696
henryR <henry11snake@hotmail.com>
2024-05-28 10:48:34 -0500

    Se corrige error al momento de subir a railway de llamada al servicio de agentesServices, cuyo archivo estaba la primera letra en mayúsculas

--- src/services/AgentesService.js
+++ src/services/AgentesService.js
@@ -1,17 +1,9 @@
-/** Servicios para Control de Contratos
- * @module Servicios_Agentes_Soporte
- */
-
 const boom = require('@hapi/boom');
 //Se debe usar la llamada reservada por sequelize "models"
 const { models } = require('../libs/sequelize'); //Usando ORM PARA USAR MODELOS
 
 /** Clase para ejectuar los diferentes servicios para Agentes de soporte */
 class AgentesServices {
-  /** Listar todos los Agentes de soporte, mediante la función findAll() de sequelize.
-   * @see https://sequelize.org/docs/v6/core-concepts/model-querying-finders/#findall
-   * @return {Array<Object>} Devuelve un Array de objetos, con todos los datos de cada agente desde el modelo de sequelize
-   */
   async find(query) {
     const { cedagent } = query;
 
@@ -26,21 +18,11 @@ class AgentesServices {
     return answer;
   }
 
-  /** Crear nuevo Agente, se utiliza la función create() de sequelize
-   * @see https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#simple-insert-queries
-   * @param {Object} data Objeto con toda la información del agente a crear
-   * @return {Object} Devuelve un mensaje de confirmación con el objeto creado
-   */
   async create(data) {
     const newRegister = await models.Agentes.create(data);
     return { message: 'Registro creado con éxito', newRegister };
   }
 
-  /** Busqueda por el id del agente, mediante la función findByPk() de sequelize
-   * @see https://sequelize.org/docs/v6/core-concepts/model-querying-finders/#findbypk
-   * @param {string} id Id del agente a buscar
-   * @return {Object} Devuelve el objeto con los datos del agente encontrado
-   */
   async filterId(id) {
     const answer = await models.Agentes.findByPk(id);
     if (!answer) {
@@ -49,23 +31,12 @@ class AgentesServices {
     return answer;
   }
 
-  /** Actualiza información del agente, mediante la función update() de sequelize
-   * @see https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#simple-update-queries
-   * @param {string} id Id del agente a buscar para su actualización
-   * @param {Object} changes Objeto con los datos a actualizar
-   * @return {Object} Devuelve el objeto con los datos del registro actualizado
-   */
   async update(id, changes) {
     const findReg = await this.filterId(id);
     const dataUpdate = await findReg.update(changes);
     return { message: 'Datos actualizados', dataUpdate };
   }
 
-  /** Eliminar Agente, mediante la función destroy() de sequelize
-   * @see https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#simple-delete-queries
-   * @param {string} id Id del contato a eliminar
-   * @return {Object} Devuelve mensaje con el id del registro eliminado
-   */
   async delete(id) {
     const findReg = await this.filterId(id);
     await findReg.destroy();
